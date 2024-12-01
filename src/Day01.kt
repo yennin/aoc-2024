68cf72469f9ebd7d2eb01058bc930f22231fc6f8
Patrick Rüegg <patsch@patsch.info>
2024-12-01 07:47:52 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
+import kotlin.math.abs
+
 fun main() {
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var (first, second) = splitInput(input)
+
+        first = first.sorted()
+        second = second.sorted()
+        var distance = 0
+        for (i in first.indices) {
+            distance += abs(first[i] - second[i])
+        }
+        return distance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (first, second) = splitInput(input)
+        return first.sumOf { firstNumber -> second.count { it == firstNumber } * firstNumber }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun splitInput(input: List<String>) = input.map {
+    val split = it.split("   ")
+    Pair(split[0].trim().toInt(), split[1].trim().toInt())
+}.unzip()
+
+
